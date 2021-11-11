e42a60bcee1a8b446856fb4e125e875929358824
SBilousko <s.bilousko@ukr.net>
2021-11-11 18:06:19 +0200

    Task 3

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    filename: "build.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
